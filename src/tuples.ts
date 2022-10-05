0dcd2f9f9b6bef6af4f0f6ad3206a75a713ea863
Hao Chen <hche8093@uni.sydney.edu.au>
2022-10-05 16:55:20 +1100

    TS tuples

---
+++ src/tuples.ts
@@ -0,0 +1,9 @@
+// define tuple
+let ourTuple: [number, boolean, string];
+
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
+
+// tuple should be readonly
+let readonlyTuple: readonly [number, boolean, string];
+readonlyTuple = [1, true, 'Hello World'];
