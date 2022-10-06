14c50eefbf7c1c0fa1b75aa83949e4aa43382148
Hao Chen <hche8093@uni.sydney.edu.au>
2022-10-06 16:18:36 +1100

    TS interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,20 @@
+// Interfaces are similar to type aliases, except they only apply to object types.
+interface Rectangle {
+  height: number,
+  width: number
+}
+
+const rectangle: Rectangle = {
+  height: 20,
+  width: 10
+}
+
+interface ColorRectangle extends Rectangle {
+  color: string
+}
+
+const colorRectangle: ColorRectangle = {
+  height: 20,
+  width: 10,
+  color: "red"
+}
