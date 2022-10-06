6e6df6c42f38803e5dde7be860341817c5dcf181
Hao Chen <hche8093@uni.sydney.edu.au>
2022-10-06 16:11:35 +1100

    TS aliases

---
+++ src/aliases.js
@@ -0,0 +1,10 @@
+var carYear = 2001;
+var carType = "Toyota";
+var carModel = "Corolla";
+// can not define same variable in different files?
+var carAlias = {
+    year: carYear,
+    type: carType,
+    model: carModel
+};
+console.log(carAlias);
